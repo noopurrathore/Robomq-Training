7de61b4c97b4b6522ad20f22e682f6d4bc6a3272
noopurrathore <59957740+noopurrathore@users.noreply.github.com>
2020-01-29 09:57:04 +0530

    Day6:Ecommerce(25-01-2020)

---
+++ EcommerceProj/src/com/robomq/dao/DBConnection.java
@@ -0,0 +1,19 @@
+package com.robomq.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	
+	static Connection con;
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "Nupur12345");
+			System.out.println("Connected....");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
